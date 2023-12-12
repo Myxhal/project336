0ff1ae620d7606c54df15c491a83d4e00271f570
chunt9913 <148998990+chunt9913@users.noreply.github.com>
2023-12-12 14:59:33 -0500

    Update TravelReservation.sql

--- TravelReservation.sql
+++ TravelReservation.sql
@@ -1,3 +1,53 @@
+DROP DATABASE IF EXISTS `TravelReservation`;
+CREATE DATABASE IF NOT EXISTS`TravelReservation`;
+USE `TravelReservation`;
+
+DROP TABLE IF EXISTS `airline`;
+CREATE TABLE airline(airlineID char(2) PRIMARY KEY);
+
+DROP TABLE IF EXISTS `plane`;
+CREATE TABLE Plane(planeID varchar(20), seats int, airlineID char(2),
+PRIMARY KEY (planeID));
+/*FOREIGN KEY (airlineID) REFERENCES Airline(airlineID));*/
+
+DROP TABLE IF EXISTS `airport`;
+CREATE TABLE airport(AirportID char(3) PRIMARY KEY);
+
+DROP TABLE IF EXISTS `customerRep`;
+CREATE TABLE customerRep(ID varchar(10), password varchar (20),
+PRIMARY KEY (ID));
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE admin(ID varchar(10), password varchar(20),
+PRIMARY KEY (ID));
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE customer(ID varchar(10), password varchar(20),
+PRIMARY KEY (ID));
+
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE ticket(tid varchar(30), cid varchar(10), airline char(2), planeID varchar(20), 
+dep_airport char(3), dep_date date, dep_time time, arr_airport char(3), purchase_date date,  
+class varchar(50), fare varchar(10),
+PRIMARY KEY (tid));
+/*FOREIGN KEY (cid) REFERENCES customer(ID),
+FOREIGN KEY (airline) REFERENCESs airline(airlineID),
+FOREIGN KEY (planeID) REFERENCES plane(planeID));*/
+
+DROP TABLE IF EXISTS `flights`;
+CREATE TABLE flights(FID varchar(20) PRIMARY KEY, airline char(2), planeid varchar(30),
+dep_airport char(3), dep_date date, dep_time time, arr_airport char(3), arr_date date, arr_time time, 
+f_type varchar(20), stops int, eco_price int, bus_price int, first_price int, seats int, Domestic varchar(20));
+/*FOREIGN KEY (depid) REFERENCES Airport(AirportID),
+FOREIGN KEY (destid) REFERENCES Airport(AirportID),
+FOREIGN KEY (departPlane) REFERENCES Plane(planeID),
+FOREIGN KEY (destPlane) REFERENCES Plane(planeID));*/
+
+DROP TABLE IF EXISTS `questions`;
+CREATE TABLE questions(cID varchar(10), question varchar(100),
+PRIMARY KEY(cID, question));
+/*FOREIGN KEY (cID) REFERENCES customer(ID));*/
+
 DROP TABLE IF EXISTS `replies`;
 CREATE TABLE replies(ID varchar(10), question varchar(100), 
 repID varchar(10), reply varchar(100),
