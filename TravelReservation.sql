0146bc85caa902d0d54a79ba91a754410b16bff9
chunt9913 <148998990+chunt9913@users.noreply.github.com>
2023-12-12 14:59:01 -0500

    Update TravelReservation.sql

--- TravelReservation.sql
+++ TravelReservation.sql
@@ -1,53 +1,3 @@
-DROP DATABASE IF EXISTS `TravelReservation`;
-CREATE DATABASE IF NOT EXISTS`TravelReservation`;
-USE `TravelReservation`;
-
-DROP TABLE IF EXISTS `airline`;
-CREATE TABLE airline(airlineID char(2) PRIMARY KEY);
-
-DROP TABLE IF EXISTS `plane`;
-CREATE TABLE Plane(planeID varchar(20), seats int, airlineID char(2),
-PRIMARY KEY (planeID));
-/*FOREIGN KEY (airlineID) REFERENCES Airline(airlineID));*/
-
-DROP TABLE IF EXISTS `airport`;
-CREATE TABLE airport(AirportID char(3) PRIMARY KEY);
-
-DROP TABLE IF EXISTS `customerRep`;
-CREATE TABLE customerRep(ID varchar(10), password varchar (20),
-PRIMARY KEY (ID));
-
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE admin(ID varchar(10), password varchar(20),
-PRIMARY KEY (ID));
-
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE customer(ID varchar(10), password varchar(20),
-PRIMARY KEY (ID));
-
-DROP TABLE IF EXISTS `ticket`;
-CREATE TABLE ticket(tid varchar(10), cid varchar(10), airline char(2), planeID varchar(20), 
-dep_airport char(3), dep_date date, dep_time time, arr_airport char(3), purchase_date date,  
-class varchar(50), fare varchar(10), 
-PRIMARY KEY (tid));
-/*FOREIGN KEY (cid) REFERENCES customer(ID),
-FOREIGN KEY (airline) REFERENCES airline(airlineID),
-FOREIGN KEY (planeID) REFERENCES plane(planeID));*/
-
-DROP TABLE IF EXISTS `flights`;
-CREATE TABLE flights(FID varchar(20) PRIMARY KEY, airline char(2), planeid varchar(30),
-dep_airport char(3), dep_date date, dep_time time, arr_airport char(3), arr_date date, arr_time time, 
-f_type varchar(20), stops int, eco_price float, bus_price float, first_price float, seats int, Domestic varchar(20));
-/*FOREIGN KEY (depid) REFERENCES Airport(AirportID),
-FOREIGN KEY (destid) REFERENCES Airport(AirportID),
-FOREIGN KEY (departPlane) REFERENCES Plane(planeID),
-FOREIGN KEY (destPlane) REFERENCES Plane(planeID));*/
-
-DROP TABLE IF EXISTS `questions`;
-CREATE TABLE questions(cID varchar(10), question varchar(100),
-PRIMARY KEY(cID, question));
-/*FOREIGN KEY (cID) REFERENCES customer(ID));*/
-
 DROP TABLE IF EXISTS `replies`;
 CREATE TABLE replies(ID varchar(10), question varchar(100), 
 repID varchar(10), reply varchar(100),
@@ -55,6 +5,10 @@ PRIMARY KEY (ID, question, reply));
 /*FOREIGN KEY (ID) REFERENCES customer(ID),
 FOREIGN KEY (repID) REFERENCES customerRep(ID),
 FOREIGN KEY (question) references questions(question));*/
+
+DROP TABLE IF EXISTS `waitingList`;
+CREATE TABLE waitingList(ID varchar(10), airline char(2), planeid varchar(30), dep_date date,
+PRIMARY KEY (ID, airline, planeid, dep_date));
 
 
 insert into customer(ID, password) 
@@ -79,4 +33,10 @@ values ('admin1', 'adminpass');
 
 select * from admin;
 select * from ticket;
+select * from flights;
+
+insert into flights(fid, airline, planeid, dep_airport, dep_date, dep_time, arr_airport, arr_date, arr_time, f_type, stops,
+eco_price, bus_price, first_price, seats, domestic)
+values('8b472', 'UD', 'fh873', 'PHL', '2023-12-30', '11:30:00', 'TPA', '2023-12-30', '14:15:00', 
+'One Way', 0, 150, 350, 500, 20, 'Domestic');
 select * from flights;
