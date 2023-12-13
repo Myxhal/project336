fdd577f6067937a394689b45cf027b666399020f
chunt9913 <148998990+chunt9913@users.noreply.github.com>
2023-12-13 15:44:01 -0500

    Update TravelReservation.sql

--- TravelReservation.sql
+++ TravelReservation.sql
@@ -8,7 +8,6 @@ CREATE TABLE airline(airlineID char(2) PRIMARY KEY);
 DROP TABLE IF EXISTS `plane`;
 CREATE TABLE Plane(planeID varchar(20), seats int, airlineID char(2),
 PRIMARY KEY (planeID));
-/*FOREIGN KEY (airlineID) REFERENCES Airline(airlineID));*/
 
 DROP TABLE IF EXISTS `airport`;
 CREATE TABLE airport(AirportID char(3) PRIMARY KEY);
@@ -30,35 +29,55 @@ CREATE TABLE ticket(tid varchar(30), cid varchar(10), airline char(2), planeID v
 dep_airport char(3), dep_date date, dep_time time, arr_airport char(3), purchase_date date,  
 class varchar(50), fare varchar(10),
 PRIMARY KEY (tid));
-/*FOREIGN KEY (cid) REFERENCES customer(ID),
-FOREIGN KEY (airline) REFERENCESs airline(airlineID),
-FOREIGN KEY (planeID) REFERENCES plane(planeID));*/
 
 DROP TABLE IF EXISTS `flights`;
 CREATE TABLE flights(FID varchar(20) PRIMARY KEY, airline char(2), planeid varchar(30),
 dep_airport char(3), dep_date date, dep_time time, arr_airport char(3), arr_date date, arr_time time, 
 f_type varchar(20), stops int, eco_price int, bus_price int, first_price int, seats int, Domestic varchar(20));
-/*FOREIGN KEY (depid) REFERENCES Airport(AirportID),
-FOREIGN KEY (destid) REFERENCES Airport(AirportID),
-FOREIGN KEY (departPlane) REFERENCES Plane(planeID),
-FOREIGN KEY (destPlane) REFERENCES Plane(planeID));*/
 
 DROP TABLE IF EXISTS `questions`;
 CREATE TABLE questions(cID varchar(10), question varchar(100),
 PRIMARY KEY(cID, question));
-/*FOREIGN KEY (cID) REFERENCES customer(ID));*/
 
 DROP TABLE IF EXISTS `replies`;
 CREATE TABLE replies(ID varchar(10), question varchar(100), 
 repID varchar(10), reply varchar(100),
 PRIMARY KEY (ID, question, reply));
-/*FOREIGN KEY (ID) REFERENCES customer(ID),
-FOREIGN KEY (repID) REFERENCES customerRep(ID),
-FOREIGN KEY (question) references questions(question));*/
 
 DROP TABLE IF EXISTS `waitingList`;
 CREATE TABLE waitingList(ID varchar(10), airline char(2), planeid varchar(30), dep_date date,
 PRIMARY KEY (ID, airline, planeid, dep_date));
+
+
+insert into customer(ID, password) 
+values('1001', 'password');
+
+insert into customer(ID, password)
+values('1002', 'finalproject');
+
+insert into customer(ID, password)
+values('1003', 'part3');
+
+insert into replies(id, question, repid, reply)
+values('1001', 'Hi how are you?', 'repdeez', "Terrible");
+
+select * from replies WHERE reply LIKE '%ter%';
+
+insert into ticket(tid, cid, airline, planeid, dep_airport, dep_date, dep_time, arr_airport, purchase_date, class, fare)
+values('1234', '1001', 'UD', 'nxb7ef', 'LAX', '2023-12-29', '2:00:00', 'EWR', '2023-11-23', 'First Class', 315);
+
+insert into admin(id, password)
+values ('admin1', 'adminpass');
+
+select * from admin;
+select * from ticket;
+select * from flights;
+
+insert into flights(fid, airline, planeid, dep_airport, dep_date, dep_time, arr_airport, arr_date, arr_time, f_type, stops,
+eco_price, bus_price, first_price, seats, domestic)
+values('8b472', 'UD', 'fh873', 'PHL', '2023-12-30', '11:30:00', 'TPA', '2023-12-30', '14:15:00', 
+'One Way', 0, 150, 350, 500, 20, 'Domestic');
+select * from flights;
 
 -- Sample data for airline table
 INSERT INTO airline (airlineID) VALUES ('AA');
@@ -94,15 +113,15 @@ INSERT INTO customer (ID, password) VALUES ('user3', 'userpass3');
 INSERT INTO ticket (tid, cid, airline, planeID, dep_airport, dep_date, dep_time, arr_airport, purchase_date, class, fare)
 VALUES
     ('T001', 'user1', 'AA', 'PL001', 'JFK', '2023-01-01', '12:00:00', 'LAX', '2023-01-01', 'Economy', '100.00'),
-    ('T002', 'user2', 'UA', 'PL002', 'LAX', '2023-02-01', '14:00:00', 'ORD', '2023-02-01', 'Business', '200.00'),
+    ('T002', 'user2', 'UA', 'PL002', 'LAX', '2023-02-01', '22:00:00', 'ICN', '2023-02-01', 'Business', '1200.00'),
     ('T003', 'user3', 'DL', 'PL003', 'ORD', '2023-03-01', '10:00:00', 'JFK', '2023-03-01', 'First Class', '300.00');
 
 -- Sample data for flights table
 INSERT INTO flights (FID, airline, planeid, dep_airport, dep_date, dep_time, arr_airport, arr_date, arr_time, f_type, stops, eco_price, bus_price, first_price, seats, Domestic)
 VALUES
-    ('F001', 'AA', 'PL001', 'JFK', '2023-01-01', '12:00:00', 'LAX', '2023-01-01', '15:00:00', 'Direct', 0, 150, 300, 500, 150, 'Yes'),
-    ('F002', 'UA', 'PL002', 'LAX', '2023-02-01', '14:00:00', 'ORD', '2023-02-01', '17:00:00', 'Direct', 0, 200, 400, 600, 200, 'No'),
-    ('F003', 'DL', 'PL003', 'ORD', '2023-03-01', '10:00:00', 'JFK', '2023-03-01', '13:00:00', 'Direct', 0, 250, 450, 700, 180, 'Yes');
+    ('F001', 'AA', 'PL001', 'JFK', '2023-01-01', '12:00:00', 'LAX', '2023-01-01', '15:00:00', 'One Way', 0, 150, 300, 500, 150, 'Domestic'),
+    ('F002', 'UA', 'PL002', 'LAX', '2023-02-01', '14:00:00', 'ICN', '2023-02-01', '22:00:00', 'Round Trip', 0, 1200, 1400, 1600, 200, 'International'),
+    ('F003', 'DL', 'PL003', 'ORD', '2023-03-01', '10:00:00', 'JFK', '2023-03-01', '13:00:00', 'Round Trip', 0, 250, 450, 700, 180, 'Domestic');
 
 -- Sample data for questions table
 INSERT INTO questions (cID, question) VALUES ('user1', 'How can I change my seat?');
@@ -118,64 +137,3 @@ INSERT INTO replies (ID, question, repID, reply) VALUES ('user3', 'Can I bring m
 INSERT INTO waitingList (ID, airline, planeid, dep_date) VALUES ('user1', 'AA', 'PL001', '2023-02-01');
 INSERT INTO waitingList (ID, airline, planeid, dep_date) VALUES ('user2', 'UA', 'PL002', '2023-03-01');
 INSERT INTO waitingList (ID, airline, planeid, dep_date) VALUES ('user3', 'DL', 'PL003', '2023-04-01');
-
-
-
-
-
-
-insert into airline(airlineID)
-values('SW');
-insert into airline(airlineID)
-values('UA');
-insert into airline(airlineID)
-values('JB');
-
-
-insert into Plane(planeID, seats, )
-
-Plane(planeID varchar(20), seats int, airlineID char(2),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-insert into customer(ID, password) 
-values('1001', 'password');
-
-insert into customer(ID, password)
-values('1002', 'finalproject');
-
-insert into customer(ID, password)
-values('1003', 'part3');
-
-
-insert into replies(id, question, repid, reply)
-values('1001', 'Hi how are you?', 'repdeez', "Terrible");
-
-select * from replies WHERE reply LIKE '%ter%';
-
-insert into ticket(tid, cid, airline, planeid, dep_airport, dep_date, dep_time, arr_airport, purchase_date, class, fare)
-values('1234', '1001', 'UD', 'nxb7ef', 'LAX', '2023-12-29', '2:00:00', 'EWR', '2023-11-23', 'First Class', 315);
-
-insert into admin(id, password)
-values ('admin1', 'adminpass');
-
-select * from admin;
-select * from ticket;
-select * from flights;
-
-insert into flights(fid, airline, planeid, dep_airport, dep_date, dep_time, arr_airport, arr_date, arr_time, f_type, stops,
-eco_price, bus_price, first_price, seats, domestic)
-values('8b472', 'UD', 'fh873', 'PHL', '2023-12-30', '11:30:00', 'TPA', '2023-12-30', '14:15:00', 
-'One Way', 0, 150, 350, 500, 20, 'Domestic');
-select * from flights; */
